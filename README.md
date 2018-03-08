83982792fbe167f11a27a2408bac4847bbf85e06
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2018-03-08 16:57:16 +0100

    More stable

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # adresses-cadastre
 Adresses ouvertes extraites du cadastre
+
+INSTABLE !!
+
+## Installation
+
+```
+yarn global add @etalab/adresses-cadastre
+```
+
+## Utilisation
+
+```
+cat ~/data/majic-2018/departements/73/BATI.gz | cadastre2adresses --dep 73 --fantoirPath ~/data/fantoir --pciPath ~/data/etalab-cadastre/2017-10-12
+```
---
+++ bin/cadastre2adresses.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+const {resolve} = require('path')
+const argv = require('yargs').argv
+const {parse} = require('@etalab/majic')
+const {stringify} = require('geojson-stream')
+//
+const extract = require('../lib/extract')
+
+if (!argv.dep) {
+  boom('Le paramètre `--dep` doit être fourni pour procéder à l’extraction')
+}
+if (!argv.fantoirPath) {
+  boom('Le paramètre `--fantoirPath` doit être fourni pour procéder à l’extraction')
+}
+if (!argv.pciPath) {
+  boom('Le paramètre `--pciPath` FANTOIR doit être fourni pour procéder à l’extraction')
+}
+
+process.stdin
+  .pipe(parse({profile: 'simple'}))
+  .pipe(extract({
+    departement: argv.dep,
+    pciPath: resolve(argv.pciPath),
+    fantoirPath: resolve(argv.fantoirPath)
+  }))
+  .pipe(stringify())
+  .pipe(process.stdout)
+
+process.on('unhandledRejection', (reason, p) => {
+  console.error('Unhandled Rejection at:', p, 'reason:', reason)
+})
+
+function boom(message) {
+  console.error(message)
+  process.exit(1)
+}
